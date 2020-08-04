57da6f9935c70e216b21c4837c96b75c2f4cbec4
ChoHJin <cjc9739@naver.com>
2020-08-04 16:23:48 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://Chohj:qwer1234@ham1.ci4ar.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true,
+    useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
