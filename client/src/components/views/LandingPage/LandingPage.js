26168706f107aa2822b110e23556dd0ca215a947
ChoHJin <cjc9739@naver.com>
2020-08-11 14:04:04 +0900

    완
    auth기능 구현 끝

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
-
+import { withRouter } from 'react-router-dom';
 
 function LandingPage(props) {
 
@@ -26,7 +26,6 @@ function LandingPage(props) {
             width : '100%', height : '100vh'
         }} >
             <h2>시작 페이지</h2>
-
             <button onClick={onClickHandler}>
                 로그아웃
             </button>
@@ -34,4 +33,4 @@ function LandingPage(props) {
     )
 }
 
-export default LandingPage
+export default withRouter(LandingPage)
